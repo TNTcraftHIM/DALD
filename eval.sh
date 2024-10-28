f46c0029165f548e453ab89ff64d4b8860fb6979
cong-zeng <congzeng66@gmail.com>
2024-10-28 05:38:45 +0000

    update readme

--- eval.sh
+++ eval.sh
@@ -3,9 +3,9 @@
 target_model_name="GPT-4"
 reference_model_name="llama3-8b"
 scoring_model_name="llama3-8b"
-weight_path="/home/dongk/dkgroup/congzeng/DALD/ckpt/checkpoint-31"
+weight_path="./ckpt/checkpoint-31"
 eval_dataset_name="Pubmed"
-eval_dataset_file="/home/dongk/dkgroup/congzeng/DALD/exp_gpt4-0613/data/pubmed_gpt-4-0613"
+eval_dataset_file="./exp_gpt4-0613/data/pubmed_gpt-4-0613"
 
 python scripts/eval_dald.py --reference_model_name ${reference_model_name} --scoring_model_name ${scoring_model_name} \
     --weight_path ${weight_path} --target_model_name ${target_model_name} --eval_dataset_name ${eval_dataset_name} \
